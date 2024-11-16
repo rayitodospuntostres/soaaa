bbc4eac1f5cfe52be4334ba7886b0122d5e04bf9
rayitodospuntostres <https://github.com/rayitodospuntostres/pruebagit.git>
2024-11-16 11:56:33 -0500

    Auto-update: Sat Nov 16 11:56:33 HPS 2024

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductosComponent } from './modules/principal/productos/productos.component';
+import { CabeceraComponent } from './modules/principal/cabecera/cabecera.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent
   ],
   imports: [
     BrowserModule,
