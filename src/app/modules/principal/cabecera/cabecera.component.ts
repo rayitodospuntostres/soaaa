2a1cd4164c851e31724a92643cb79ec417bedfc8
Georgui Azel <azelps071del@gmail.com>
2024-12-07 22:48:12 -0500

    feat: Add hot sale banner and free sales components; integrate Firebase configuration

--- src/app/modules/principal/cabecera/cabecera.component.ts
+++ src/app/modules/principal/cabecera/cabecera.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
@@ -7,22 +8,37 @@ import { AuthService } from 'src/app/services/auth/auth.service';
   templateUrl: './cabecera.component.html',
   styleUrls: ['./cabecera.component.scss']
 })
-export class CabeceraComponent {
+export class CabeceraComponent implements OnInit{
 
   username: string = 'Iniciar sesion ';
+
+  usuarioActual: any;
   
-  constructor(private authSerivce: AuthService,private router:Router) {
+  constructor(
+    private authSerivce: AuthService,
+    private router: Router,
+    private afAuth: AngularFireAuth
+  ) {
     const user = localStorage.getItem('user');
     if (user) {
       const userObj = JSON.parse(user);
-      if (userObj && userObj.username) {
-        this.username = userObj.username;
+      if (userObj) {
+        this.username = userObj.username || userObj.displayName || 'Iniciar sesion';
       }
+    } else {
+      this.username = 'Iniciar sesion';
     }
   }
+  
 
   logout(){
     this.authSerivce.logout();
     this.router.navigate(['/']);
+  }
+
+  ngOnInit(): void {
+
+
+   
   }
 }
--- src/app/modules/principal/principal.module.ts
+++ src/app/modules/principal/principal.module.ts
@@ -6,6 +6,8 @@ import { InicioComponent } from './inicio/inicio.component';
 import { ProductosComponent } from './productos/productos.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
 import { PaymentComponent } from './payment/payment.component';
+import { HotSaleBannerComponent } from './hot-sale-banner/hot-sale-banner.component';
+import { HotSalesFreeComponent } from './hot-sales-free/hot-sales-free.component';
 
 
 @NgModule({
